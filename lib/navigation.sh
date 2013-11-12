76fa995e31137ae708917a04bb43d4de7acf6215
Adam Tavares <adamtavares@YukonCornelius.local>
2013-11-12 16:59:36 -0500

    Added documentation.

--- lib/navigation.sh
+++ lib/navigation.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# This could be obnoxious
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+##-------------------------------------------------------------
+##  SCRIPT_DIR -- Running script's absolute path
+##
+##	Often you will want to know the absolute path of the
+##  running script so you can use relative paths in your
+##  command statements.  This isn't a function but a variable.
+##  Use like so...
+##  	$SCRIPT_DIR/child/path
+##      $SCRIPT_DIR/../sibling/path
+##-------------------------------------------------------------
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
