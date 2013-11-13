95fe675c677c106ea9142ae65a6c1174ab0a416d
Adam Tavares <adamtavares@YukonCornelius.local>
2013-11-13 10:13:55 -0500

    changed writeConfig function to write to STDOUT by default.

--- lib/templating.sh
+++ lib/templating.sh
@@ -4,18 +4,17 @@
 ##  writeConfig() -- Simple templating
 ##
 ##	Replace all instances of ${VAR} with the value of $VAR
+##  and write output stream to STDOUT
 ##
 ##  @param {string} The path to the template file
-##  @param {string} The output file
 ##-------------------------------------------------------------
 function writeConfig() {
-	> $2  # Clobber output file
 	while read line ; do
 	    while [[ "$line" =~ '(\$\{[a-zA-Z_][a-zA-Z_0-9]*\})' ]] ; do
 	        LHS=${BASH_REMATCH[1]}
 	        RHS="$(eval echo "\"$LHS\"")"
 	        line=${line//$LHS/$RHS}
 	    done
-	    echo $line >> $2
+	    echo $line
 	done < $1
 }
