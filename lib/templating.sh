f459f056f791fc73944242abd7c3b839fcc34408
caesarfeta <adamtavares@gmail.com>
2013-11-18 11:55:38 -0500

    changed snippets to a text file so they don't mess with actual script variables.

--- lib/templating.sh
+++ lib/templating.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#-------------------------------------------------------------
+#  Import config
+#-------------------------------------------------------------
+source /tmp/bashlibvars
+
 ##-------------------------------------------------------------
 ##  writeConfig() -- Simple templating
 ##
@@ -8,8 +13,7 @@
 ##  @param {string} The path to the template file
 ##  @param {string} The output file
 ##
-##  Be careful the output file gets clobbered!
-##  That means totally overwritten
+##  Be careful the output file gets totally overwritten.
 ##-------------------------------------------------------------
 function writeConfig() {
 	> $2  # Clobber output file
@@ -23,5 +27,27 @@ function writeConfig() {
 			fi
 	    done
 	    echo $line >> $2
+	done < $1
+}
+
+##-------------------------------------------------------------
+##  writeManual() -- Simple templating useful for turning
+##  admin scripts into something resembling documentation.
+##
+##	Replace all instances of $VAR with the value of $VAR
+##
+##  @param {string} The path to the template file
+##-------------------------------------------------------------
+function writeManual() {
+	while read line ; do
+	    while [[ "$line" =~ (\$\{*[a-zA-Z_][a-zA-Z_0-9]*\}*) ]] ; do
+	        LHS=${BASH_REMATCH[1]}
+			if [ -n $LHS ]
+				then
+					RHS="$(eval echo "\"$LHS\"")"
+					line=${line//$LHS/$RHS}
+			fi
+	    done
+	    echo $line
 	done < $1
 }
